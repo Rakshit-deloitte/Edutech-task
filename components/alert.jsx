c067ae20533648f94ebb65d8fb72f0ec89e098f6
Rakshit <rmyalameli@deloitte.com>
2022-07-04 11:30:24 +0530

    Added styling component for mui.

--- components/alert.jsx
+++ components/alert.jsx
@@ -1,11 +1,28 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
+import { makeStyles } from '@mui/styles';
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 
+const useStyles = makeStyles({
+  root: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    borderRadius: 3,
+    border: 0,
+    color: "white",
+    height: 48,
+    padding: "0 30px !important",
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+  },
+  label: {
+    textTransform: "capitalize",
+  },
+});
+
 export default function AlertDialog(props) {
+  const classes = useStyles();
   const { handleAlertDailog, open, body } = props;
   return (
     <div>
@@ -21,7 +38,15 @@ export default function AlertDialog(props) {
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleAlertDailog}>Close</Button>
+          <Button
+            classes={{
+              root: classes.root, // class name, e.g. `classes-nesting-root-x`
+              label: classes.label, // class name, e.g. `classes-nesting-label-x`
+            }}
+            onClick={handleAlertDailog}
+          >
+            Close
+          </Button>
         </DialogActions>
       </Dialog>
     </div>
